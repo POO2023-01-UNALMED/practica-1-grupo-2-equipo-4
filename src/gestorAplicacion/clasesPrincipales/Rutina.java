0de3554cba4b9bb009f41eb4bedb7490e94b331f
jaco8ar <124937427+jaco8ar@users.noreply.github.com>
2023-04-27 16:41:32 -0500

    Clase Rutina

--- src/gestorAplicacion/clasesPrincipales/Rutina.java
+++ src/gestorAplicacion/clasesPrincipales/Rutina.java
@@ -1,5 +1,37 @@
 package gestorAplicacion.clasesPrincipales;
 
-public class Rutina {
+import java.io.Serializable;
+import java.util.ArrayList;
 
+import gestorAplicacion.clasesHerencia.Movimiento;
+
+public class Rutina implements Serializable{
+    private static final long serialVersionUID = 1L;
+	String name;
+
+    public Rutina(String name) {this.name = name;}
+
+    private ArrayList<Ejercicio> ejercicios = new ArrayList<Ejercicio>();
+    private TipoEjercicio tipoEjercicio;
+    private final int NUMEJERCIOS = 7; // abierto a discusión
+
+    public Rutina(ArrayList<Ejercicio> ejercicios, TipoEjercicio tipoEjercicio){
+        this.ejercicios = ejercicios;
+        this.tipoEjercicio = tipoEjercicio;
+    }
+
+
+
+    //Metodos get y set
+    public void setEjercicios (ArrayList<Ejercicio> ejercicios) {
+        this.ejercicios = ejercicios;}
+    public void setTipoEjercicio (TipoEjercicio tipoEjercicio) {
+        this.tipoEjercicio = tipoEjercicio;}
+    
+    public ArrayList<Ejercicio> getEjercicios() {
+        return ejercicios;}
+    public TipoEjercicio getTipoEjercicio() {
+        return tipoEjercicio;}
+    
+    
 }
