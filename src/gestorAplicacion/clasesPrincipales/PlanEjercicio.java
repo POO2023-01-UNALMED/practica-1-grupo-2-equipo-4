d7d1d43549caa7e3db9341fd551aff2caa770a0d
jaco8ar <124937427+jaco8ar@users.noreply.github.com>
2023-04-27 16:41:03 -0500

    Update PlanEjercicio.java

--- src/gestorAplicacion/clasesPrincipales/PlanEjercicio.java
+++ src/gestorAplicacion/clasesPrincipales/PlanEjercicio.java
@@ -1,5 +1,15 @@
 package gestorAplicacion.clasesPrincipales;
 
-public class PlanEjercicio {
+import java.util.HashMap;
 
+import gestorAplicacion.clasesHerencia.Plan;
+
+public class PlanEjercicio implements Plan{
+    String nombre;
+    private HashMap<String, Rutina> planSemanalEjercio = new HashMap<String, Rutina>();
+
+    public PlanEjercicio(String nombre, HashMap<String, Rutina> planSemanalEjercio){
+        this.nombre = nombre;
+        this.planSemanalEjercio = planSemanalEjercio;
+    }
 }
