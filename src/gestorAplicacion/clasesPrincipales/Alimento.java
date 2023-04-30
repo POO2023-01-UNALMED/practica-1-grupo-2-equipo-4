0674d209c751faabd29ae3b60b6f3f1d04c1767d
JuanOspina05 <juospinae@unal.edu.co>
2023-04-30 00:28:44 -0500

    cambios Juan Ospina

--- src/gestorAplicacion/clasesPrincipales/Alimento.java
+++ src/gestorAplicacion/clasesPrincipales/Alimento.java
@@ -1,5 +1,59 @@
 package gestorAplicacion.clasesPrincipales;
 
 public class Alimento {
+    private String nombre;
+    private double calorias;
+    private double proteinas;
+    private double carbohidratos;
+    private double grasas;
 
+    // Constructor
+    public Alimento(String nombre, double calorias, double proteinas, double carbohidratos, double grasas) {
+        this.nombre = nombre;
+        this.calorias = calorias;
+        this.proteinas = proteinas;
+        this.carbohidratos = carbohidratos;
+        this.grasas = grasas;
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(double calorias) {
+        this.calorias = calorias;
+    }
+
+    public double getProteinas() {
+        return proteinas;
+    }
+
+    public void setProteinas(double proteinas) {
+        this.proteinas = proteinas;
+    }
+
+    public double getCarbohidratos() {
+        return carbohidratos;
+    }
+
+    public void setCarbohidratos(double carbohidratos) {
+        this.carbohidratos = carbohidratos;
+    }
+
+    public double getGrasas() {
+        return grasas;
+    }
+
+    public void setGrasas(double grasas) {
+        this.grasas = grasas;
+    }
 }
--- src/gestorAplicacion/clasesPrincipales/Comida.java
+++ src/gestorAplicacion/clasesPrincipales/Comida.java
@@ -1,5 +1,110 @@
 package gestorAplicacion.clasesPrincipales;
 
 public class Comida {
+    private String nombre;
+    public Alimento[] listaAlimentos;
+    public double calorias;
+    public double proteinas;
+    public double carbohidratos;
+    public double grasas;
+    public TipoComida tipo;
+    private PreferenciaAlimenticia preferenciaAlimenticia;
+    public Cliente objetivoCliente;
 
+    // Constructor, getters y setters
+    public Comida(String nombre, Alimento[] listaAlimentos, TipoComida tipo, PreferenciaAlimenticia preferenciaAlimenticia, Cliente objetivoCliente) {
+        this.nombre = nombre;
+        this.listaAlimentos = listaAlimentos;
+        this.tipo = tipo;
+        this.preferenciaAlimenticia = preferenciaAlimenticia;
+        this.objetivoCliente = objetivoCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Alimento[] getListaAlimentos() {
+        return listaAlimentos;
+    }
+
+    public void setListaAlimentos(Alimento[] listaAlimentos) {
+        this.listaAlimentos = listaAlimentos;
+    }
+
+    public double getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(double calorias) {
+        this.calorias = calorias;
+    }
+
+    public double getProteinas() {
+        return proteinas;
+    }
+
+    public void setProteinas(double proteinas) {
+        this.proteinas = proteinas;
+    }
+
+    public double getCarbohidratos() {
+        return carbohidratos;
+    }
+
+    public void setCarbohidratos(double carbohidratos) {
+        this.carbohidratos = carbohidratos;
+    }
+
+    public double getGrasas() {
+        return grasas;
+    }
+
+    public void setGrasas(double grasas) {
+        this.grasas = grasas;
+    }
+
+    public TipoComida getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoComida tipo) {
+        this.tipo = tipo;
+    }
+
+    public PreferenciaAlimenticia getPreferenciaAlimenticia() {
+        return preferenciaAlimenticia;
+    }
+
+    public void setPreferenciaAlimenticia(PreferenciaAlimenticia preferenciaAlimenticia) {
+        this.preferenciaAlimenticia = preferenciaAlimenticia;
+    }
+
+    public Cliente getObjetivoCliente() {
+        return objetivoCliente;
+    }
+
+    public void setObjetivoCliente(Cliente objetivoCliente) {
+        this.objetivoCliente = objetivoCliente;
+    }
+
+    public void calcularCalorias() {
+        // logica
+    }
+
+    public void calcularMacronutrientes() {
+        // logica
+    }
+
+    public void filtrarComidasPorObjetivo() {
+        // logica
+    }
+
+    public void filtrarComidasPorPreferenciaAlimenticia() {
+        // logica
+    }
 }
