205aec944bef7abc3ee0680e40d272052cc613f2
juanma01rs <juanma10rs@gmail.com>
2023-04-29 19:09:22 -0500

    cambios

--- src/gestorAplicacion/clasesHerencia/Movimiento.java
+++ src/gestorAplicacion/clasesHerencia/Movimiento.java
@@ -1,5 +1,52 @@
 package gestorAplicacion.clasesHerencia;
 
-public class Movimiento {
+import java.io.Serializable;
 
+public class Movimiento implements Serializable {
+	private static final long serialVersionUID = 1L; 
+	protected String nombre;
+	protected String descripcion;
+	protected String musculoPrincipal;
+	protected Maquina maquina;
+	protected boolean completado;
+	
+	public static filtrarMovimientosPorTipo {
+
+	}
+	
+	public void setNombre(String nombre) {
+	    this.nombre = nombre;
+	}
+	public String getNombre() {
+	    return nombre;
+	}
+
+	public void setDescripcion(String descripcion) {
+	    this.descripcion = descripcion;
+	}
+	public String getDescripcion() {
+	    return descripcion;
+	}
+
+	public void setMusculoPrincipal(String musculoPrincipal) {
+	    this.musculoPrincipal = musculoPrincipal;
+	}
+	public String getMusculoPrincipal() {
+	    return musculoPrincipal;
+	}
+
+	public void setMaquina(Maquina maquina) {
+	    this.maquina = maquina;
+	}
+	public Maquina getMaquina() {
+	    return maquina;
+	}
+
+	public void setCompletado(boolean completado) {
+	    this.completado = completado;
+	}
+
+	public boolean isCompletado() {
+	    return completado;
+	}
 }
--- src/gestorAplicacion/clasesPrincipales/Maquina.java
+++ src/gestorAplicacion/clasesPrincipales/Maquina.java
@@ -1,5 +1,39 @@
 package gestorAplicacion.clasesPrincipales;
 
-public class Maquina {
+import java.io.Serializable;
 
+public class Maquina implements Serializable {
+	private static final long serialVersionUID = 1L; 
+	private String nombre;
+	private Gimnasio gimnasio;
+	private Movimiento movimientoRealizado;
+	
+	
+	public static disponibilidad {
+		
+	}
+	
+	public void setNombre(String nombre) {
+	    this.nombre = nombre;
+	}
+
+	public String getNombre() {
+	    return nombre;
+	}
+
+	public void setGimnasio(Gimnasio gimnasio) {
+	    this.gimnasio = gimnasio;
+	}
+
+	public Gimnasio getGimnasio() {
+	    return gimnasio;
+	}
+
+	public void setMovimientoRealizado(Movimiento movimientoRealizado) {
+	    this.movimientoRealizado = movimientoRealizado;
+	}
+
+	public Movimiento getMovimientoRealizado() {
+	    return movimientoRealizado;
+	}
 }
