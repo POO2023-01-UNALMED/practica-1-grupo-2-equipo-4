a29e7378e6ac80298ea31b5ddbd4f1222f52efe2
vasquez-esteban <124936891+vasquez-esteban@users.noreply.github.com>
2023-06-06 12:42:43 -0500

    Objetos y Gestionador (Nuevo Main)
    se sube el modelo de objetos a crear y el main donde va la interfaz gráfica.

---
+++ Python/src/guiMain/gestionador.py
@@ -0,0 +1,15 @@
+# import tkinter as tk
+#
+# # Aquí va la interfaz Gráfica
+#
+# window = tk.Tk()
+# window.title("Gymbro")
+# window.geometry("450x300")
+# window.mainloop()
+
+# Aquí también se crean los objetos
+
+
+
+
+
