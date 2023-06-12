ef902831fa3f841893744929a78ee33be70cb0b6
juanma01rs <124938043+juanma01rs@users.noreply.github.com>
2023-06-12 18:02:01 -0500

    Add files via upload

---
+++ Python/src/gestorAplicacion/clasesPrincipales/Cliente.py
@@ -0,0 +1,116 @@
+from src.gestorAplicacion.clasesHerencia.Persona import Persona
+from src.gestorAplicacion.clasesPrincipales.Gimnasio import Gimnasio
+from src.gestorAplicacion.clasesPrincipales.Historial import Historial
+from src.gestorAplicacion.clasesPrincipales.Entrenador import Entrenador
+from src.gestorAplicacion.clasesPrincipales.PlanEjercicio import PlanEjercicio
+from src.gestorAplicacion.clasesPrincipales.PlanAlimentacion import PlanAlimentacion
+from src.gestorAplicacion.clasesEnum.NivelCliente import NivelCliente
+from src.gestorAplicacion.clasesEnum.ObjetivoCliente import ObjetivoCliente
+from src.gestorAplicacion.clasesEnum.PreferenciaAlimenticia import PreferenciaAlimenticia
+
+
+# Autores: Esteban Vásquez Pérez.
+
+# Finalidad de la clase: 
+# La clase cliente representa una de las Personas que está asistiendo a nuestros Gimnasios
+# El cliente es el usuario principal de la aplicación. 
+# Tiene acceso a todas las funcionalidades a través de su número de identificación.
+
+# Estructuras de datos:
+# Utilizamos un lista para guardar cada uno de los clientes
+
+# Componentes:
+# Atributos heredados de Persona: nombre, gimnasio, identificación.
+# Atributos que describen el físico del cliente
+# Un objeto de entrenador
+
+# Una serie de descripciones del cliente que incluyen
+# preferencia alimenticia (por ejemplo si es vegano)
+# nivel cliente que puede ser (PRINCIPIANTE, INTERMEDIO, AVANZADO)
+# Un objetivo particular del cliente (por ejemplo bajar de peso)
+# un PlanAlimentacion que el cliente sigue en este momento.
+# un Plan que va a representar el PlanEjercicio
+# un Historial de planes que contiene
+# historial de gimnasios visitados
+# historial de planes de ejercicio y alimentación
+# un historial que tiene los gimnasios que ha visitado y la cantidad de veces que ha ido.
+
+
+class Cliente(Persona):
+    listaClientes = []
+
+    def __init__(self, 
+                nombre: str, 
+                gimnasio: Gimnasio, 
+                identificacion: int, 
+                altura: float, 
+                peso: float, 
+                sexo: str, 
+                edad: int, 
+                entrenador: Entrenador,
+                preferenciaAlimenticia: PreferenciaAlimenticia, 
+                nivelCliente: NivelCliente,
+                objetivoCliente: ObjetivoCliente):
+        
+        super().__init__(nombre, 
+                        gimnasio, 
+                        identificacion)
+
+        self.altura = altura
+        self.peso = peso
+        self.sexo = sexo
+        self.edad = edad
+        self.entrenador = entrenador
+        self.preferenciaAlimenticia = preferenciaAlimenticia
+        self.nivelCliente = nivelCliente
+        self.objetivoCliente = objetivoCliente
+        self.planAlimentacion = None
+        self.planEjercicio = None
+        self.historialPlanes = None
+
+        self.gimnasio.agregarCliente(self)
+        self.listaClientes.append(self)
+
+
+    def __str__(self):
+        return "Nombre: {}\nGimnasio: {}\nNivel: {}\nEntrenador: {}\nPlan Alimentacion: {}".format(
+            self.nombre, 
+            str(self.gimnasio), 
+            self.nivelCliente.value, 
+            self.entrenador.nombre,
+            self.planAlimentacion.nombrePlan
+        )
+    
+    def asignarEntrenador(self, entrenador):
+        self.entrenador = entrenador
+        entrenador.cliente = self
+        self.entrenador.disponibilidad = "NO DISPONIBLE"
+
+    def asignarPlanAlimentacion(self, planAlimentacion):
+        self.planAlimentacion = planAlimentacion
+
+        if self.historialPlanes is None:
+            historialParaPlanesAlimentacion = Historial(self)
+            self.historialPlanes = historialParaPlanesAlimentacion
+            self.historialPlanes.agregarPlanAlimentacion(planAlimentacion)
+
+        else:
+            self.historialPlanes.agregarPlanAlimentacion(planAlimentacion)
+
+    def asignarPlanEjercicio(self, planEjercicio):
+        self.planEjercicio = planEjercicio
+
+        if self.historialPlanes is None:
+            historialParaPlanesEjercicio = Historial(self)
+            self.historialPlanes = historialParaPlanesEjercicio
+            self.historialPlanes.agregarPlanEjercicio(planEjercicio)
+
+        else:
+            self.historialPlanes.agregarPlanEjercicio(planEjercicio)
+
+    def generarPlanEjercicio(self):
+        plan = PlanEjercicio()
+        plan.dificultad = self.nivelCliente
+        self.planEjercicio = plan.crearPlanSemanal(self.objetivoCliente)
+        return str(self.planEjercicio)
+
--- Python/src/gestorAplicacion/clasesPrincipales/Entrenador.py
+++ Python/src/gestorAplicacion/clasesPrincipales/Entrenador.py
@@ -3,7 +3,8 @@ from src.gestorAplicacion.clasesPrincipales import Cliente, Gimnasio
 from src.gestorAplicacion.clasesEnum import NivelCliente
 import random
 
-# Autores: Esteban Vásquez Pérez.
+# Autores: Esteban Vásquez Pérez y
+#          Juan Manuel Rodríguez S.
 
 # Finalidad de la clase:
 # La clase Entrenador sirve para crear objetos de los entrenadores que pertenecen a
@@ -30,7 +31,7 @@ class Entrenador(Persona):
         self.formacion = formacion
         self.cliente = cliente
         self.nivelEntrenador = nivelEntrenador
-        self.disponibilidad = disponibilidad # MAÑANA/TARDE/NO DISPONIBLE
+        self.disponibilidad = disponibilidad
 
         self.calificaciones = self.generarListaCalificaciones()
         self.calificacionPromedio = self.calcularCalificacionPromedio(self.calificaciones)
@@ -40,7 +41,6 @@ class Entrenador(Persona):
         self.gimnasio.agregarEntrenador(self)
         Entrenador.listaEntrenadores.append(self)
 
-    # Funciones para efectos de la creación de objetos
     def generarListaCalificaciones(self):
         calificaciones = [random.randint(1, 5) for _ in range(10, random.randint(10, 20))]
         return calificaciones
@@ -49,9 +49,7 @@ class Entrenador(Persona):
         promedio = round(sum(calificaciones) / len(calificaciones), 1)
         return promedio
 
-    # Método de funcionalidad
     def filtroEntrenadores(self, cliente):
-        # Filtro de un entrenador que se acomode a las necesidades del cliente
 
         entrenadorFiltrado = None
 
@@ -74,16 +72,13 @@ class Entrenador(Persona):
 
         return entrenadorFiltrado
 
-    # Métodos para mostrar información en pantalla
     def __str__(self):
         return f"Nombre: {self.nombre}\nGimnasio: {self.gimnasio}\nNivel: {self.nivelEntrenador}\nCliente: {self.cliente.nombre}\nDisponibilidad: {self.disponibilidad}\n"
 
     def descripcionHojaVida(self):
         return f"{self.nombre}\nCalificacion Promedio: {self.calificacionPromedio}\nEdad: {self.edad}\nFormacion: {self.formacion}\nNivel: {self.nivelEntrenador}\nGimnasio: {self.gimnasio}\nDisponibilidad: {self.disponibilidad}\n"
 
-    # Método de funcionalidad
     def entrenadoresDisponibles(self, horarioAsistencia, intensidad):
-        # Filtro de entrenadores por intensidad y horario de asistencia adecuado
         entrenadorDisponible = None
         if self.nivelEntrenador == intensidad and self.disponibilidad == horarioAsistencia:
             entrenadorDisponible = self
